f7840ce85b70063828800ef395fe9576a5ba39e3
Luiz Gustavo Coelho Xavier <luizgustavocx1981@gmail.com>
2025-09-10 20:24:31 -0300

    Create lista1\exercicio7.java

---
+++ lista1\exercicio7.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class exercicio7
+{
+	public static void main(String[] args) {
+		System.out.printnl("digite a base do retangulo");
+		
+		double base = nextDouble();
+		
+		System.out.print("digite a altura do retangulo");
+		
+		double altura = nextDouble();
+		
+		double area = base * atura;
+		
+			System.out.printnl("a area dp retangulo é: ");
+			
+			sc.close();
+		
+		
+		
+		
+	}
+}
