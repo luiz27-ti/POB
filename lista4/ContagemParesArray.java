9691fdfbda10617f222fa56f26c6a8f64c41bad0
Luiz Gustavo Coelho Xavier <luizgustavocx1981@gmail.com>
2025-09-17 10:27:19 -0300

    Create ContagemParesArray.java

---
+++ lista4/ContagemParesArray.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ContagemParesArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numeros = new int[8];
+        int contadorPares = 0;
+
+        System.out.println("Digite 8 números inteiros:");
+        for (int i = 0; i < 8; i++) {
+            System.out.print("Número " + (i + 1) + ": ");
+            numeros[i] = scanner.nextInt();
+        }
+
+        for (int i = 0; i < 8; i++) {
+            if (numeros[i] % 2 == 0) {
+                contadorPares++;
+            }
+        }
+
+        System.out.println("Quantidade de números pares: " + contadorPares);
+        scanner.close();
+    }
+}
