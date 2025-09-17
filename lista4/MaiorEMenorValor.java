aff81377655847fc6b71a4972649d3df6b013451
Luiz Gustavo Coelho Xavier <luizgustavocx1981@gmail.com>
2025-09-17 10:21:12 -0300

    Create MaiorEMenorValor.java

---
+++ lista4/MaiorEMenorValor.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class MaiorEMenorValor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numeros = new int[5];
+        
+        System.out.println("Digite 5 números inteiros:");
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Número " + (i + 1) + ": ");
+            numeros[i] = scanner.nextInt();
+        }
+
+        int maior = numeros[0];
+        int menor = numeros[0];
+
+        for (int i = 1; i < 5; i++) {
+            if (numeros[i] > maior) {
+                maior = numeros[i];
+            }
+            if (numeros[i] < menor) {
+                menor = numeros[i];
+            }
+        }
+
+        System.out.println("O maior número é: " + maior);
+        System.out.println("O menor número é: " + menor);
+
+        scanner.close();
+    }
+}
